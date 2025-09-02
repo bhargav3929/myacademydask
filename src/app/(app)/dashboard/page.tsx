4a3f5c274ba5f4d58a42358452931f3a933ccfdd
Bhargav K <bhargav17748@gmail.com>
2025-09-02 10:30:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -46,7 +46,7 @@ export default function DashboardPage() {
      // Listener for recent registrations (last 5)
     const recentRegQuery = query(
         collectionGroup(firestore, "students"),
-        orderBy("joinDate", "desc"),
+        orderBy("createdAt", "desc"),
         limit(5)
     );
     const recentRegUnsubscribe = onSnapshot(recentRegQuery, snapshot => {
